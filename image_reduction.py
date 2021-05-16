f3c8611af6719c5442c111ae55e3680bb1673472
OldGentleman <dzmitry.baltramiayuk@gmail.com>
2021-05-16 10:28:37 +0200

    chore: image class

--- image_reduction.py
+++ image_reduction.py
@@ -12,12 +12,41 @@ from sklearn.decomposition import PCA, KernelPCA
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MaxAbsScaler
 
 from util.read_image_file import read_images_to_df
 
-target_food, images_food = read_images_to_df('data/raw_data/food.zip', 'images/', (512, 512))
+target_food, images_food = read_images_to_df(
+    'data/raw_data/food.zip', 'images/', (512, 512))
 
-food_image_umap = umap.UMAP(n_components = 2, n_neighbors = 4)
-reduced_foods_images = food_image_umap.fit_transform(images_food)
+print('Encoding')
+le = LabelEncoder()
+le.fit(target_food)
+target_food = le.transform(target_food)
 
-print(reduced_foods_images)
+train_food, test_food, train_target, test_target = train_test_split(
+    images_food, target_food, test_size=0.2, random_state=228)
+
+print('UMAP')
+food_image_umap = umap.UMAP(n_components=2, n_neighbors=4)
+reduced_train = food_image_umap.fit_transform(train_food)
+reduced_test = food_image_umap.transform(test_food)
+
+
+print('max abs encoding')
+
+max_abs = MaxAbsScaler().fit(reduced_train)
+reduced_train = max_abs.transform(reduced_train)
+reduced_test = max_abs.transform(reduced_test)
+
+print('SVM')
+svc_noramal = SVC(gamma='scale', C=1)
+svc_reduced = SVC(gamma='scale', C=1)
+
+svc_noramal.fit(train_food, train_target)
+svc_reduced.fit(reduced_train, train_target)
+
+print(svc_noramal.score(test_food, test_target))
+print(svc_reduced.score(reduced_test, test_target))
--- util/read_image_file.py
+++ util/read_image_file.py
@@ -5,21 +5,23 @@ from skimage.io import imread
 from skimage.transform import resize
 from typing import Tuple
 
-def read_images_to_df(file_location: str, pattern: str, target_size : Tuple[int, int]):
-    df = pd.DataFrame(columns=['class', 'image'])
+
+def read_images_to_df(file_location: str, pattern: str, target_size: Tuple[int, int]):
+    classes = []
+    images = []
     with zipfile.ZipFile(file_location, 'r') as z:
         i = 0
         for file in z.namelist():
             if file.startswith(pattern):
                 ifile = z.open(file)
-                #optional set as_gray=True
+                # optional set as_gray=True
                 image = imread(ifile, as_gray=True)
                 if image.shape[0] != target_size[0] or image.shape[1] != target_size[1]:
                     image = resize(image, target_size, anti_aliasing=True)
-                df.loc[i] = [ifile.name.split('/')[1]] + [image.ravel()]
+                classes.append(ifile.name.split('/')[1])
+                images.append(image.ravel())
                 i += 1
 
-    return df['class'].values, np.stack(df['image'].values)
-
-
-food_images = read_images_to_df('data/raw_data/food.zip', 'images/', (512, 512))
+    image_matrix = np.stack(images)
+    del images
+    return classes,  image_matrix
