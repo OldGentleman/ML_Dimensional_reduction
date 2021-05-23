bd5370aa61ed45cffd6edee42312aff6a2c17876
OldGentleman <dzmitry.baltramiayuk@gmail.com>
2021-05-23 13:18:30 +0200

    chore: flow images

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ import warnings
 warnings.filterwarnings("ignore")
 
 from flow import flow
-from read.read_all_data import read_all_preprocessed_data
+from read.read_all_data import read_all_preprocessed_data, read_images
 
 
 random_state = 228
 
 
 for label, train, file_name in read_all_preprocessed_data():
+    results = flow(train, label, dim_num=2, cv=10, random_state=random_state)
+    results.to_csv(f'data/results/{file_name}_results.csv', index=False)
+
+for label, train, file_name in read_images():
     results = flow(train, label, dim_num=2, cv=10, random_state=random_state)
     results.to_csv(f'data/results/{file_name}_results.csv', index=False)
